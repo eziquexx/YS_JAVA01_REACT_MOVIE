72bf1c35d876c228c0f9ff71670ffbf2d3b8e5a5
jelee <waftyann@gmail.com>
2024-07-29 14:29:07 +0900

    step05_네비게이션 연결

--- src/step05/App.js
+++ src/step05/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Detail from './pages/Detail';
+import { Navigation } from './components/Navigation';
 
 function App() {
   
   return (
     <BrowserRouter>
+      <Navigation />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
