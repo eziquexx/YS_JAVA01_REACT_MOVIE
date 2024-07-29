6fbd09b778f9b7ba9b332fe904a65908ce3195c1
jelee <waftyann@gmail.com>
2024-07-29 14:39:02 +0900

    step06 about style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './step05/App';
+import App from './step06/App';
 // import AppEx from './step03/AppEx';
 import reportWebVitals from './reportWebVitals';
 
---
+++ src/step06/pages/Detail.jsx
@@ -0,0 +1,13 @@
+import './Detail.css';
+
+
+function Detail() {
+
+  return (
+    <div className='about_container'>
+      <span>상세페이지</span>
+    </div>
+  );
+}
+
+export default Detail;
