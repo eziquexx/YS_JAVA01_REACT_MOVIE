780160612d16b4dde375104bece25a245a82e16d
jelee <waftyann@gmail.com>
2024-07-29 12:37:49 +0900

    step04_영화제목읽어오기2

---
+++ src/App2.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { useState, useEffect } from "react";
+
+
+// 1. useState 사용하여 boolean값 담을 변수 생성.
+// 2. useState 사용하여 count 값 담을 변수 생성.
+
+
+
+function App2() {
+  
+
+  return (
+    <div>
+
+
+    </div>
+  );
+}
